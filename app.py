21a08bc93845a24eea272aba9304391c6cd7c69f
baguamaster <traderkev54@yahoo.com>
2021-06-16 20:01:17 +0000

    debugging without mongo

--- app.py
+++ app.py
@@ -2,21 +2,50 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
+from Recipe import Recipe
+
+# Sample Recipe to test without MongoDB
+sample = Recipe(
+    _id = "00001",
+    cuisine_type = "Singaporean",
+    recipe_name = "Chicken Rice",
+    meal_time = ["Lunch"],
+    description = "Rice with Steamed Chicken",
+    servings = 1,
+    is_vegetarian = "No",
+    prep_time = "10",
+    cooking_time = "30",
+    ingredients = {
+        "Rice": "100g",
+        "Chicken": "50g"
+    },
+    method = {
+        "1": "Cook rice",
+        "2": "Cook chicken",
+        "3": "Plate"
+    },
+    allergens = ["Nuts"],
+    image = "https://picsum.photos/200/300",
+    video = "https://www.youtube.com",
+    recipe_by = "John"
+)
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo(app)
+# Temp
+app.secret_key = "SECRET"
 
+# mongo = PyMongo(app)
 
 '''
 `All Recipes` Template Rendering
@@ -26,8 +55,9 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/recipes")
 def recipes():
-    recipes = list(mongo.db.recipes.find())
-    recipes.sort(key=lambda k: k['recipe_name'])
+    recipes=[sample,sample]
+    # recipes = list(mongo.db.recipes.find())
+    # recipes.sort(key=lambda k: k['recipe_name'])
     return render_template("recipes.html", recipes=recipes)
 
 
@@ -38,7 +68,8 @@ def recipes():
 
 @app.route("/recipes/<recipe_id>", methods=["GET", "POST"])
 def full_recipe(recipe_id):
-    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    recipe=sample
+    # recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("full_recipe.html", recipe=recipe)
 
 
@@ -50,29 +81,39 @@ Register User Functionality & Template Rendering
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # Checking if username already exists in the DB as lowercase:
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-
-        if existing_user:
+        # Test for existing_user
+        if request.form.get("username") == "Jonny":
             flash("Username already exists")
             return redirect(url_for("register"))
 
-        # Checking if email already exists in the DB:
-        existing_email = mongo.db.users.find_one(
-            {"email": request.form.get("email")})
-
-        if existing_email:
+        # Test for exisiting_email
+        if request.form.get("email") == "john@gmail.com":
             flash("Email already exists")
             return redirect(url_for("register"))
 
+        # Checking if username already exists in the DB as lowercase:
+        # existing_user = mongo.db.users.find_one(
+        #     {"username": request.form.get("username").lower()})
+
+        # if existing_user:
+        #     flash("Username already exists")
+        #     return redirect(url_for("register"))
+
+        # Checking if email already exists in the DB:
+        # existing_email = mongo.db.users.find_one(
+        #     {"email": request.form.get("email")})
+
+        # if existing_email:
+        #     flash("Email already exists")
+        #     return redirect(url_for("register"))
+
         # If username does not exist, then store in db:
-        register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password")),
-            "email": request.form.get("email")
-        }
-        mongo.db.users.insert_one(register)
+        # register = {
+        #     "username": request.form.get("username").lower(),
+        #     "password": generate_password_hash(request.form.get("password")),
+        #     "email": request.form.get("email")
+        # }
+        # mongo.db.users.insert_one(register)
 
         # After clicking register, user will now be in session:
         session["user"] = request.form.get("username").lower()
@@ -89,30 +130,45 @@ User Login Functionality & Template Rendering
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # Checking if username already exists in the DB as lowercase:
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-
-        if existing_user:
-            # Checking if the hashed password matches the user's input:
-            if check_password_hash(
-                existing_user["password"],
-                    request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-                return redirect(url_for(
-                        "profile", username=session["user"]))
-            else:
-                # If the invalid password is incorrect then alert user.
-                # For security reasons message displays either username
-                # or password is incorrect. This minimises brute-forcing.
-                flash("Incorrect Username and/or Password")
-                return redirect(url_for("login"))
-
-        else:
-            # If the username does not exist then display the same message
-            # as if the password was incorrect.
+        # Test if username does not exist
+        if request.form.get("username").lower() == "jonny":
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
+
+        # Test if incorrect password
+        if request.form.get("password") == "11111":
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+        # Test if username exists
+        session["user"] = request.form.get("username").lower()
+        return redirect(url_for("profile", username=session["user"]))
+
+        # Checking if username already exists in the DB as lowercase:
+        # existing_user = mongo.db.users.find_one(
+        #     {"username": request.form.get("username").lower()})
+
+        # if existing_user:
+            # Checking if the hashed password matches the user's input:
+            # if check_password_hash(
+            #     existing_user["password"],
+            #         request.form.get("password")):
+            #     session["user"] = request.form.get("username").lower()
+            #     return redirect(url_for(
+            #             "profile", username=session["user"]))
+            # else:
+            #     # If the invalid password is incorrect then alert user.
+            #     # For security reasons message displays either username
+            #     # or password is incorrect. This minimises brute-forcing.
+            #     flash("Incorrect Username and/or Password")
+            #     return redirect(url_for("login"))
+
+        # else:
+        #     # If the username does not exist then display the same message
+        #     # as if the password was incorrect.
+        #     flash("Incorrect Username and/or Password")
+        #     return redirect(url_for("login"))
+        return render_template("login.html")
 
     return render_template("login.html")
 
@@ -137,11 +193,12 @@ User Profile Template Rendering
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    recipes=[sample, sample]
     # The session user's username is retrieved from MongoDB
-    username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
-    recipes = list(mongo.db.recipes.find(
-        {"recipe_by": session["user"]}))
+    # username = mongo.db.users.find_one(
+    #     {"username": session["user"]})["username"]
+    # recipes = list(mongo.db.recipes.find(
+    #     {"recipe_by": session["user"]}))
     return render_template(
         "profile.html", username=username,
         recipes=recipes)
@@ -154,8 +211,9 @@ Search Functionality
 
 @app.route("/search")
 def search():
-    query = request.args.get("query", "-minutes -mins")
-    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    recipes=[sample]
+    # query = request.args.get("query", "-minutes -mins")
+    # recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
     return render_template("recipes.html", recipes=recipes)
 
 
@@ -192,23 +250,23 @@ def new_recipe():
         allergens = request.form.get("allergens")
         allergens_list = [x.strip() for x in allergens.split(",")]
 
-        recipe = {
-            "cuisine_type": request.form.get("cuisine_type"),
-            "recipe_name": request.form.get("recipe_name"),
-            "meal_time": request.form.getlist("meal_time"),
-            "description": request.form.get("description"),
-            "servings": request.form.get("servings"),
-            "is_vegetarian": is_vegetarian,
-            "prep_time": request.form.get("prep_time"),
-            "cooking_time": request.form.get("cooking_time"),
-            "ingredients": ingredients,
-            "method": method_obj,
-            "allergens": allergens_list,
-            "image": request.form.get("image"),
-            "video": request.form.get("video"),
-            "recipe_by": session["user"]
-        }
-        mongo.db.recipes.insert_one(recipe)
+        # recipe = {
+        #     "cuisine_type": request.form.get("cuisine_type"),
+        #     "recipe_name": request.form.get("recipe_name"),
+        #     "meal_time": request.form.getlist("meal_time"),
+        #     "description": request.form.get("description"),
+        #     "servings": request.form.get("servings"),
+        #     "is_vegetarian": is_vegetarian,
+        #     "prep_time": request.form.get("prep_time"),
+        #     "cooking_time": request.form.get("cooking_time"),
+        #     "ingredients": ingredients,
+        #     "method": method_obj,
+        #     "allergens": allergens_list,
+        #     "image": request.form.get("image"),
+        #     "video": request.form.get("video"),
+        #     "recipe_by": session["user"]
+        # }
+        # mongo.db.recipes.insert_one(recipe)
         flash("Thank you! Your recipe has been successfully added.")
         return redirect(url_for("recipes"))
 
@@ -264,10 +322,11 @@ def edit_recipe(recipe_id):
             "video": request.form.get("video"),
             "recipe_by": session["user"]
         }
-        mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, submit)
+        # mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, submit)
         flash("Recipe Successfully Updated")
 
-    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    recipe=None
+    # recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("edit_recipe.html", recipe=recipe)
 
 
@@ -278,7 +337,7 @@ def edit_recipe(recipe_id):
 
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
-    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    # mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
     flash("Recipe Successfully Deleted")
     return redirect(url_for("recipes"))
 
@@ -294,6 +353,7 @@ def cooking_tools():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=False)
+    # app.run(host=os.environ.get("IP"),
+    #         port=int(os.environ.get("PORT")),
+    #         debug=False)
+    app.run()
